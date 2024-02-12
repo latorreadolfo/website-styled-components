e2c3ec0e50c63a494d8487044e45f707143b9fb1
latorreadolfo <adolfo3eng@gmail.com>
2024-02-12 17:53:18 -0300

    Navbar resources

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,8 +5,12 @@ import {
   NavLogo,
   NavIcon,
   MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
 } from "./Navbar.elements";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
@@ -15,16 +19,30 @@ const Navbar = () => {
 
   return (
     <>
-      <Nav>
-        <NavbarWrapper>
-          <NavLogo to="/">
-            <NavIcon />
-          </NavLogo>
-          <MobileIcon onClick={handleClick}>
-            {click ? <FaTimes /> : <FaBars />}
-          </MobileIcon>
-        </NavbarWrapper>
-      </Nav>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav>
+          <NavbarWrapper>
+            <NavLogo to="/">
+              <NavIcon />
+              JOHNSON
+            </NavLogo>
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+            <NavMenu onClick={handleClick} click={click}>
+              <NavItem>
+                <NavLinks to="/">Home</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/services">Services</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/products">Products</NavLinks>
+              </NavItem>
+            </NavMenu>
+          </NavbarWrapper>
+        </Nav>
+      </IconContext.Provider>
     </>
   );
 };
