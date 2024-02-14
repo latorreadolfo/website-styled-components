3e915d4ef39bbfe267636b10f3d0c51529df3347
latorreadolfo <adolfo3eng@gmail.com>
2024-02-14 11:40:25 -0300

    new elements and creating footer

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -10,7 +10,58 @@ export const homeObjOne = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
   headline: "Passionate Programmer",
   topLine: "Lorem Ipsum",
-  img: require("../../images/undraw_code_review_re_woeb.svg"),
+  img: require("../../images/svg-1.svg"),
+  alt: "Image",
+  start: "",
+};
+
+export const homeObjTwo = {
+  lightBg: true,
+  primary: false,
+  imgStart: "start",
+  lightTopLine: true,
+  lightTextDesc: false,
+  lightText: false,
+  buttonLabel: "Contact me",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+  headline: "Passionate Programmer",
+  topLine: "Lorem Ipsum",
+  img: require("../../images/profile.jpg"),
+  alt: "Image",
+  start: "true",
+};
+
+export const homeObjThree = {
+  lightBg: false,
+  primary: true,
+  imgStart: "",
+  lightTopLine: true,
+  lightTextDesc: true,
+  lightText: true,
+  buttonLabel: "Contact me",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+  headline: "Passionate Programmer",
+  topLine: "Lorem Ipsum",
+  img: require("../../images/svg-2.svg"),
+  alt: "Image",
+  start: "",
+};
+
+export const homeObjFour = {
+  lightBg: false,
+  primary: true,
+  imgStart: "",
+  lightTopLine: true,
+  lightTextDesc: true,
+  lightText: true,
+  buttonLabel: "Contact me",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+  headline: "Passionate Programmer",
+  topLine: "Lorem Ipsum",
+  img: require("../../images/svg-3.svg"),
   alt: "Image",
   start: "",
 };
