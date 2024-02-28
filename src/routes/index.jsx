95d44935e33ec29f94c150359e1fd0c2645679fa
latorreadolfo <adolfo3eng@gmail.com>
2024-02-28 13:15:24 -0300

    docs: fixed some directory problems

---
+++ src/routes/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import GlobalStyle from "../globalStyles";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../pages/HomePage/Home";
+import Services from "../pages/Services/Services";
+import Products from "../pages/Products/Products";
+import SignUp from "../pages/SignUp/SignUp";
+import { Navbar, Footer, ScrollToTop } from "../components";
+
+function InitRoutes() {
+  return (
+    <Router>
+      <GlobalStyle />
+      <ScrollToTop />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default InitRoutes;
